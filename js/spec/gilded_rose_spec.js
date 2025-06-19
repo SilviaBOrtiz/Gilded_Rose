7890cd7a263a0eed9ad8757a0f36b050ffc01875
SilviaBOrtiz <silviariasortiz@gmail.com>
2025-06-19 17:55:05 +0100

    Erste. Fix des Dummy-Tests

--- js/spec/gilded_rose_spec.js
+++ js/spec/gilded_rose_spec.js
@@ -1,9 +1,10 @@
 describe("Gilded Rose", function() {
 
-  it("should foo", function() {
-    items = [ new Item("foo", 0, 0) ];
+  it("should degrade normal items by 1 before sell_in", function() {
+    items = [ new Item("+5 Dexterity Vest", 10, 20) ];
     update_quality();
-    expect(items[0].name).toEqual("fixme");
+    expect(items[0].sell_in).toBe(9);
+    expect(items[0].quality).toBe(19);
   });
-
 });
+
