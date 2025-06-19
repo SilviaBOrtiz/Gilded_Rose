b686e62dcf1696c2f3015f70a48ebb1c15eeb77e
SilviaBOrtiz <silviariasortiz@gmail.com>
2025-06-19 19:57:24 +0100

    Should not change Sulfuras

--- js/spec/gilded_rose_spec.js
+++ js/spec/gilded_rose_spec.js
@@ -42,5 +42,12 @@ describe("Gilded Rose", function() {
     expect(items[0].quality).toEqual(50);
   });
 
+  it("should not change Sulfuras", function() {
+    items = [ new Item("Sulfuras, Hand of Ragnaros", 0, 80) ];
+    update_quality();
+    expect(items[0].sell_in).toEqual(0);
+    expect(items[0].quality).toEqual(80);
+  });
+
 
 });
