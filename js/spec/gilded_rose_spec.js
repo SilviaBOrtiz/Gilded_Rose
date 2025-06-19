2bee724058f30caf107906d055b08f7bfd025197
SilviaBOrtiz <silviariasortiz@gmail.com>
2025-06-19 19:33:12 +0100

    Sellin prüfen

--- js/spec/gilded_rose_spec.js
+++ js/spec/gilded_rose_spec.js
@@ -1,10 +1,9 @@
 describe("Gilded Rose", function() {
 
-  it("should degrade normal items by 1 before sell_in", function() {
-    items = [ new Item("+5 Dexterity Vest", 10, 20) ];
+  it("should lower sellin", function() {
+    items = [ new Item("foo", 5, 0) ];
     update_quality();
-    expect(items[0].sell_in).toBe(9);
-    expect(items[0].quality).toBe(19);
+    expect(items[0].sell_in).toEqual(4);
   });
-});
 
+});
